5727d77d2bc9493f193a6fd07314a3a46fcfd570
leovolpatto <leovolpatto@gmail.com>
2019-11-04 10:42:42 -0300

    tab1 renaming

--- src/app/trabalhos/trabalhos.page.spec.ts
+++ src/app/trabalhos/trabalhos.page.spec.ts
@@ -1,19 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { Tab1Page } from './tab1.page';
+import { TrabalhosPage } from './trabalhos.page';
 
 describe('Tab1Page', () => {
-  let component: Tab1Page;
-  let fixture: ComponentFixture<Tab1Page>;
+  let component: TrabalhosPage;
+  let fixture: ComponentFixture<TrabalhosPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [Tab1Page],
+      declarations: [TrabalhosPage],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(Tab1Page);
+    fixture = TestBed.createComponent(TrabalhosPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
